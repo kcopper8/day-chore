b5277647780f04333c211351efce8c0d7b3251a4
kcopper8 <kcopper8@gmail.com>
2023-11-13 23:58:18 +0900

    feat: set init gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <>
+          <h1>Vite + React</h1>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
           </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </>
-    ),
+        </>
+      ),
+    },
+  ],
+  {
+    basename: "/day-chore/",
   },
-]);
+);
 
 function App() {
   return <RouterProvider router={router} />;
