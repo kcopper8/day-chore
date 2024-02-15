3a753b4f9fad5ed4fb17b3b1678b2da1da3041ac
kcopper8 <kcopper8@gmail.com>
2024-02-15 13:47:50 +0900

    chore: make active link bold

--- src/components/GlobalLayout/index.tsx
+++ src/components/GlobalLayout/index.tsx
@@ -1,13 +1,27 @@
+import { PropsWithChildren } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
 type GlobalLayoutProps = {};
+
+const HeaderLink = ({ to, children }: { to: string } & PropsWithChildren) => {
+  return (
+    <NavLink
+      to={to}
+      style={({ isActive }) => ({
+        fontWeight: isActive ? "bold" : "normal",
+      })}
+    >
+      {children}
+    </NavLink>
+  );
+};
 
 const GlobalLayout = ({}: GlobalLayoutProps) => {
   return (
     <>
       <header>
-        <NavLink to={"/"}>Today</NavLink> |{" "}
-        <NavLink to={"/archive"}>Archive</NavLink>
+        <HeaderLink to={"/"}>Toay</HeaderLink> |{" "}
+        <HeaderLink to={"/archive"}>Archive</HeaderLink>
       </header>
       <Outlet />
     </>
